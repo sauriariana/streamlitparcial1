8ab8eaa207713af6b16d0294f0f2a6a5e47c8ec3
sauriariana <114454113+sauriariana@users.noreply.github.com>
2022-09-26 20:48:12 -0500

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+
 dfcalificaciones = pd.read_excel("tareas.xlsx")
 
 dfcalificaciones["promedio"] = (dfcalificaciones["tarea1"] + dfcalificaciones["tarea2"] + dfcalificaciones["tarea3"] + dfcalificaciones["tarea4"])/4
@@ -9,3 +11,6 @@ dfcalificaciones ["status"] = dfcalificaciones["promedio"].apply (lambda x: "apr
 dfcalificaciones
 
 st.dataframe(dfcalificaciones)
+fig = px.bar(drresultados, x='status', y=dfresultados["status".value_counts())
+st.plotly_chart(fig)
+                                                      
